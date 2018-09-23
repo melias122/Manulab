307cb4f2e789a8bed1ba9e57341c9087ad512ec7
Martin Elias <melias122@gmail.com>
2018-09-23 11:38:30 +0200

    remove qt4 check as we require qt5 to build

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,6 @@
 
 int main(int argc, char *argv[])
 {
-#if QT_VERSION < QT_VERSION_CHECK(5, 0, 0)
-	QApplication::setGraphicsSystem("raster");
-#endif
 	QApplication a(argc, argv);
 
 	// init resources
